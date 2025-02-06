dd8f3c6f19c6e0bbe1968522a4129864e86ee792
Houda <houdaharmane2004@gmail.com>
2025-02-06 13:57:24 +0100

    Ajout de la logique de connexion de db

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./user.db", (err) => {
+  if (err) {
+    console.error("Erreur de la connexion a la base de donnees" + err.message);
+  } else {
+    console.log("Connexion a la base de donnees reussie");
+  }
+});
