0fd294ced07450d2f6f4ff685c28d6cc137d69bf
Houda <houdaharmane2004@gmail.com>
2025-02-07 16:45:15 +0100

    Ajout du serveur statique pour servir les fichiers HTML, CSS et JS

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+// Servir les fichiers statiques (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route principale pour afficher `index.html`
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Connexion à la base de données SQLite
 const db = new sqlite3.Database("./user.db", (err) => {
